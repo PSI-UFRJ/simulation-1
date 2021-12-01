c9b4c7eb2392c686ff83a6331d77bcf4c57fd5d1
Tainá Lima <tainaslima19@gmail.com>
2021-12-01 19:56:12 -0300

    feat: add new sprites and started heritage organization of shapes

---
+++ Sims 1/Assets/Scripts/Circle.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour, IShape
+{
+    // Start is called before the first frame update
+    private float radius;
+    private Color color;
+    private Vector3 baseScale = new Vector3(1, 1, 1);
+    private CircleCollider2D collider;
+    private Transform transform;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        collider = this.gameObject.GetComponent<CircleCollider2D>();
+        transform = this.gameObject.GetComponent<Transform>();
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+
+        radius = collider.radius * transform.localScale.x;
+        color = spriteRenderer.color;
+    }
+
+    public void ChangeColor(Color c)
+    {
+
+    }
+
+    public void ChangeSize(float controllerValue)
+    {
+
+    }
+
+    public void ChangeArea(float a)
+    {
+
+    }
+
+    public void ChangePerimeter(float p)
+    {
+
+    }
+
+    public void Rotate(int angle = 45)
+    {
+
+    }
+
+    public float CalculateArea()
+    {
+        float area = 0;
+        return area;
+    }
+
+    public float CalculatePerimeter()
+    {
+        float perimeter = 0;
+        return perimeter;
+    }
+
+    public void Delete()
+    {
+
+    }
+}
---
+++ Sims 1/Assets/Scripts/Shape.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IShape 
+{
+    public void ChangeColor(Color c);
+    public void ChangeSize(float controllerValue);
+    public void ChangeArea(float a);
+    public void ChangePerimeter(float p);
+    public void Rotate(int angle=45);
+    public float CalculateArea();
+    public float CalculatePerimeter();
+    public void Delete();
+
+}
